73c7f5f60e7394f46fb425adf17f805a7e3c741c
rufus_alpha <jkb.millan98@gmail.com>
2023-04-04 01:39:13 +0200

    updated StartingUserInterface CreateCommand method to support Standard and Ranked GradeBooks

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,13 +34,31 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 2)
+            if (parts.Length != 3)
             {
-                Console.WriteLine("Command not valid, Create requires a name.");
+                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
             }
             var name = parts[1];
+            var type = parts[2].ToLower();
+
             BaseGradeBook gradeBook = new BaseGradeBook(name);
+
+            switch (type)
+            {
+                case "standard":
+                    gradeBook = new StandardGradeBook(name);
+                    break;
+
+                case "ranked":
+                    gradeBook = new RankedGradeBook(name);
+                    break;
+
+                default:
+                    Console.WriteLine("{0} is not a supported type of gradebook, please try again", type);
+                    return;
+            }
+
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
