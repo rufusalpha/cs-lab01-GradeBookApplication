19730684991e2d307c2680b8a7f43a5d3ab9755a
rufus_alpha <jkb.millan98@gmail.com>
2023-04-04 00:39:24 +0200

    Created class StandardGradeBook it's constructor

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
