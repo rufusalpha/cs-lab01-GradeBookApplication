95ea15761621cc6508afece5cbdcb1d435ab4068
rufus_alpha <jkb.millan98@gmail.com>
2023-04-04 01:12:08 +0200

    Added override of GetLetterGrade function

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -14,5 +14,37 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade( double averageGrade )
+        {
+            if ( Students.Count < 5 )
+            {
+                throw new InvalidOperationException("There are min. 5 students req.");
+            }
+
+            int thresholdGrade = (int)Math.Ceiling(Students.Count * 0.2);
+            var gradesList = Students.OrderByDescending(student => student.AverageGrade).Select(student => student.AverageGrade).ToList();
+
+            if (gradesList[thresholdGrade - 1] <= averageGrade)
+            {
+                return 'A';
+            }
+            else if (gradesList[(2 * thresholdGrade) - 1] <= averageGrade)
+            {
+                return 'B';
+            }
+            else if (gradesList[(3 * thresholdGrade) - 1] <= averageGrade)
+            {
+                return 'C';
+            }
+            else if (gradesList[(4 * thresholdGrade) - 1] <= averageGrade)
+            {
+                return 'D';
+            }
+            else 
+            {
+                return 'F';
+            }
+        }
     }
 }
